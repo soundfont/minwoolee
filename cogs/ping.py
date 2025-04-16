ebbb63ff9ef886dfe442a9a8e0a7b30c2c8e2e77
soundfont <164956388+soundfont@users.noreply.github.com>
2025-04-16 17:55:32 -0600

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -7,7 +7,7 @@ class Ping(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         latency = round(self.bot.latency * 1000)
-        await ctx.send(f'Pong! Latency: {latency}ms')
+        await ctx.send(f'pong! latency: {latency}ms')
 
 async def setup(bot):
     await bot.add_cog(Ping(bot))
