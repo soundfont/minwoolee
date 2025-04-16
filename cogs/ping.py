10c20b0d5fc9c017f1c6772d24d53e915eec7e0b
soundfont <164956388+soundfont@users.noreply.github.com>
2025-04-16 17:43:58 -0600

    Create ping.py

---
+++ cogs/ping.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        await ctx.send(f'Pong! Latency: {latency}ms')
+
+async def setup(bot):
+    await bot.add_cog(Ping(bot))
