2deee405f93dcf087aef3e2b6b40df9a069f4b35
soundfont <164956388+soundfont@users.noreply.github.com>
2025-04-16 00:29:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import discord
 from discord.ext import commands
 import asyncio
 
-bot = commands.Bot(command_prefix=".")
+intents = discord.Intents.default()
+intents.message_content = True  # required to read message content (for commands)
+
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 async def load():
     await bot.load_extension("instagram")  # Load Instagram cog
