0e353bb887e6a4976298182ee52a654dec73944a
soundfont <164956388+soundfont@users.noreply.github.com>
2025-04-15 23:43:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import os
 import discord
 from discord.ext import commands
 
-# set the command prefix
-bot = commands.Bot(command_prefix='.', intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True  # this is crucial
+
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'logged in as {bot.user}')
 
-# simple command: .ping
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('pong!')
 
+import os
 token = os.getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
