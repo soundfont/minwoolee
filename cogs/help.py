4488c49a1f4ec986b4f8ac14b938c9d7d9db89cb
soundfont <164956388+soundfont@users.noreply.github.com>
2025-04-16 19:07:02 -0600

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,1 +1,12 @@
+from discord.ext import commands
 
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def help(self, ctx):
+        await ctx.send(f\\'minwoolee's documentation can be found [here.](https://github.com/soundfont/minwoolee-public/blob/main/README.md)')
+
+async def setup(bot):
+    await bot.add_cog(Help(bot))
