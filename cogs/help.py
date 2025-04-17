bc9ff7ad67d73fef8ef2f66d132fc82bbc3ad754
soundfont <164956388+soundfont@users.noreply.github.com>
2025-04-16 19:08:09 -0600

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -6,7 +6,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        await ctx.send(f\\'minwoolee's documentation can be found [here.](https://github.com/soundfont/minwoolee-public/blob/main/README.md)')
+        await ctx.send(f"minwoolee's documentation can be found [here.](https://github.com/soundfont/minwoolee-public/blob/main/README.md)")
 
 async def setup(bot):
     await bot.add_cog(Help(bot))
